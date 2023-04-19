38cc4d8ff3f5144e84e0eb8c3bec987e9182b00b
Ezra Muskat <ezramuskat@gmail.com>
2023-04-19 11:08:59 -0400

    finished minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -14,7 +14,7 @@ impl Config {
             Some(arg) => arg,
             None => return Err("Didn't get a query string"),
         };
-        
+
         let file_path = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a file path"),
@@ -41,15 +41,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
-	let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+	contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a> (
